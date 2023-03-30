0b04b6169a72b90f3a3001331f2f6cc208288c47
jFroylan-Cal <j.froylan_calderon@outlook.com>
2023-03-30 17:42:58 -0600

    Jwt Strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { UnauthorizedException } from '@nestjs/common';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRespository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { id } = payload;
+    const user = await this.userRespository.findOneBy({ id });
+    if (!user) {
+      throw new UnauthorizedException('Token not valid');
+    }
+
+    if (!user.isActive) {
+      throw new Error('User is inactive, talk with an admin');
+    }
+    return user;
+  }
+}
