3760f7d43b5d87dfc25fe6f0797e53e62dd567f2
jFroylan-Cal <j.froylan_calderon@outlook.com>
2023-03-30 18:11:06 -0600

    decorators and guards for user roles

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,11 @@
+import { ValidRoles } from '../enums/valid-roles.enum';
+import { UseGuards, applyDecorators } from '@nestjs/common';
+import { RoleProtected } from './role-protected.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { UserRoleGuard } from '../guards/user-role.guard';
+export function Auth(...roles: ValidRoles[]) {
+  return applyDecorators(
+    RoleProtected(...roles),
+    UseGuards(AuthGuard, UserRoleGuard),
+  );
+}
---
+++ src/auth/decorators/raw-headers.decorator.ts
@@ -0,0 +1,9 @@
+
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const RawHeader = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.rawHeader;
+  },
+);
